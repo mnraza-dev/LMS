626c48147a7322a80d66b32e7360e5de2555c341
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-04 02:07:20 +0530

    initialize client application with React, Vite, and Tailwind CSS; add basic structure and components

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='p-4	'>
+      <Button variant='default' size={'lg'}>Let's build  LMS App</Button>
+    </div>
+  )
+}
+
+export default App
