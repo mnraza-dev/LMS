b38129162d2d49c924ee0e391340992c3f76c5de
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-04 02:17:20 +0530

    add Login page with tabs for account and password management; implement reusable UI components for labels, inputs, cards, and tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import React from "react";
+import { Button } from "./components/ui/button";
+import { Login } from "./pages/Login";
 
 const App = () => {
   return (
-    <div className='p-4	'>
-      <Button variant='default' size={'lg'}>Let's build  LMS App</Button>
+    <div className="flex  items-center justify-center h-screen space-y-4">
+      {" "}
+      '>
+      <Button variant="default" size={"lg"}>
+        Let's build LMS App
+      </Button>
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
