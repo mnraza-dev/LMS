6b1cd774583bd824a6ae1f422138cfe6220b9601
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-04 14:47:22 +0530

    add cookie-parser middleware for handling cookies; update server configuration

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './db/db.js';
 import userRoutes from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config({
@@ -11,20 +12,20 @@ dotenv.config({
 });
 
 const PORT = process.env.PORT || 5000;
+// common middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 // DB connection
 connectDB();
 
 // API routes
 app.use('/api/v1/user', userRoutes);
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
