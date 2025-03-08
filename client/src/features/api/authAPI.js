15f51e43b0be03223f0ade52df752fca7eae55ee
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-08 14:26:05 +0530

    add authAPI endpoints for user registration and login; implement token storage on login

--- client/src/features/api/authAPI.js
+++ client/src/features/api/authAPI.js
@@ -4,5 +4,35 @@ const USER_API = "http://localhost:8000/api/v1/user/";
 
 export const authAPI = createApi({
   reducerPath: "authAPI",
-  baseQuery: fetchBaseQuery({ baseUrl: USER_API, credentials: "include" }),
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: USER_API,
+    credentials: "include",
+  }),
+
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (inputData) => ({
+        url: "register/",
+        method: "POST",
+        body: inputData,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (inputData) => ({
+        url: "login/",
+        method: "POST",
+        body: inputData,
+      }),
+      async onQueryStarted({ dispatch, queryFulfilled, queryRejected }) {
+        try {
+          const result = await queryFulfilled;
+          const { token } = result.data;
+          localStorage.setItem("token", token);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
 });
